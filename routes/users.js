e7a2e746ad4d1113d3563d02f5cc0daae38d64eb
myzhou1991 <ibaiwei@yeah.net>
2016-12-01 00:49:41 +0800

    使用mongoose

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+
+mongoose.connect('localhost:27017/aixiu');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
