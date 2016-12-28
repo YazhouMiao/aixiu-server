6eaca4fbe840a4771a2e9987bed5a6128db4e70b
myzhou1991 <ibaiwei@yeah.net>
2016-12-28 23:06:58 +0800

    redis

--- redis.js
+++ redis.js
@@ -8,4 +8,14 @@ rc.on('error',function(err){
     "use strict";
     console.log('redis connect error:'+err);
 });
-rc.set('test','1',)
+rc.set('test','1');
+rc.expire('test',60);
+
+var bar = setInterval(function(){
+    rc.get('test',function(err,data){
+        console.log(data);
+        if(!data){
+            clearInterval(bar);
+        }
+    });
+},2000);
